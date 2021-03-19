3a3bd76fe922fe2dab79ad6dc983dc22b604e27e
ccrum292 <ccrum292@gmail.com>
2021-03-19 14:50:12 -0400

    added routing to other html pages

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,17 +1,42 @@
 const navData = require('../lib/navData')
 
+const data = {
+  layout: '_layout.nkj',
+  navbar: navData
+}
 
 module.exports = function(app) {
 
   app.get("/", (req, res) => {
-    const data = {
-      layout: '_layout.nkj',
-      navbar: navData
-    }
-
     res.render("index.njk", data)
   })
 
+  app.get("/about", (req, res) => {
+    res.render("about.njk", data)
+  })
 
+  app.get("/casestudy", (req, res) => {
+    res.render("case-study.njk", data)
+  })
+
+  app.get("/contact", (req, res) => {
+    res.render("contact.njk", data)
+  })
+
+  app.get("/presentation", (req, res) => {
+    res.render("presentation.njk", data)
+  })
+
+  app.get("/pricing", (req, res) => {
+    res.render("pricing.njk", data)
+  })
+
+  app.get("/services", (req, res) => {
+    res.render("services.njk", data)
+  })
+
+  app.get("/404", (req, res) => {
+    res.render("404.njk", data)
+  })
 
 }
