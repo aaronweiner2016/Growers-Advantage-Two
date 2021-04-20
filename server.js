c710e2d481757452ef87a67afb7f2fcaa460af79
aaron weiner <aaronjacobweiner@yahoo.com>
2021-04-19 21:58:42 -0400

    redirection for ssl

--- server.js
+++ server.js
@@ -4,19 +4,28 @@ const nunjucks = require('nunjucks')
 const db = require("./models");
 const app = express();
 const PORT = process.env.PORT || 8080;
-// var sslRedirect = require('heroku-ssl-redirect');
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+app.use((req, res, next) => {
+  if (process.env.NODE_ENV === 'production') {
+    if (req.headers['x-forwarded-proto'] !== 'https')
+      // the statement for performing our redirection
+      return res.redirect('https://' + req.headers.host + req.url);
+    else
+      return next();
+  } else
+    return next();
+});
+
 nunjucks.configure('views', {
   autoescape: true,
   express: app
 })
 
-// app.use(sslRedirect());
 
 require("./controllers/html-routes")(app);
 require("./controllers/api-routes")(app);
