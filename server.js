9f9a730a58b503f3cae54ff6758560d1006c6f43
aaron weiner <aaronjacobweiner@yahoo.com>
2021-04-22 10:29:18 -0400

    redirect111111

--- server.js
+++ server.js
@@ -26,6 +26,18 @@ app.use((req, res, next) => {
     return next();
 });
 
+var http = express();
+
+// set up a route to redirect http to https
+http.get('*', function (req, res) {
+  res.redirect('https://' + req.headers.host + req.url);
+
+  // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
+  // res.redirect('https://example.com' + req.url);
+})
+
+// have it listen on 8080
+
 nunjucks.configure('views', {
   autoescape: true,
   express: app
