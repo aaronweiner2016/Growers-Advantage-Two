e9f0713ba4da64fd443fc1f99724ce682aecead9
aaron weiner <aaronjacobweiner@yahoo.com>
2021-04-22 14:13:34 -0400

    404

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(function (req, res, next) {
 
   // respond with html page
   if (req.accepts('html')) {
-    res.render('404', { url: req.url });
+    res.render('404', { url: "https://www.growersadvantage.org/404" });
     return;
   }
 
