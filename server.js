a03769ef3a19b3c6a18e44dde053beac736f9284
aaron weiner <aaronjacobweiner@yahoo.com>
2021-04-22 20:18:21 -0400

    404

--- server.js
+++ server.js
@@ -27,27 +27,6 @@ app.use((req, res, next) => {
     return next();
 });
 
-app.use(function (req, res, next) {
-  res.status(404);
-
-  // respond with html page
-  if (req.accepts('html')) {
-    res.render('404', { url: req.url });
-    return;
-  }
-
-  // respond with json
-  if (req.accepts('json')) {
-    res.json({ error: 'Not found' });
-    return;
-  }
-
-  // default to plain-text. send()
-  res.type('txt').send('404.njk');
-});
-
-
-
 nunjucks.configure('views', {
   autoescape: true,
   express: app
